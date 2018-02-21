ab808f7b2b3182594e1d90e76cf3909ed8ff3754
Tomer Igal <tomer5858@gmail.com>
2018-02-21 18:34:07 +0200

    Upgrade. Added sass support, Changed folders name, Removed 'cssnano' zindex overwrite, main file from package.json.

--- rollup.config.js
+++ rollup.config.js
@@ -3,24 +3,37 @@ import babel from 'rollup-plugin-babel';
 import postcss from 'rollup-plugin-postcss';
 import cssnext from 'postcss-cssnext';
 import cssnano from 'cssnano';
+import sass from 'node-sass';
+import commonjs from 'rollup-plugin-commonjs';
+import packageJson from './package.json';
 
 export default {
-    entry: 'view/js/index.js',
-    format: 'iife',
+    input: packageJson.main,
+    external: [],
+    output: {
+        format: 'iife',
+        sourcemap: !process.env.BUILD,
+        globals: {},
+        file: 'dist/main.min.js'
+    },
     plugins: [
         postcss({
-            // sourceMap: true,
-            extensions: ['.css'],
+            sourceMap: !process.env.BUILD,
+            preprocessor: (content, id) => new Promise((resolve, reject) => {
+                const result = sass.renderSync({ file: id });
+                resolve({ code: result.css.toString() })
+            }),
+            extensions: ['.css', '.scss'],
             plugins: [
                 cssnext(),
-                cssnano({autoprefixer:false})
+                cssnano({ autoprefixer: false, zindex: false })
             ],
-            extract: 'view/css/style.min.css'
+            extract: 'dist/style.min.css'
         }),
         resolve(),
+        commonjs(),
         babel({
             exclude: 'node_modules/**' // only transpile our source code
         })
-    ],
-    dest: 'view/js/bundle.min.js'
+    ]
 };
